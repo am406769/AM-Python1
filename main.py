8ffcd5962c065af345c1e4b3b1f1d37c9ebd9c5c
am4067 <am4067@columbia.edu>
2020-09-28 17:30:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+row_number = 235
+result = row_number % 2
+print(result)
+
+name = 'Jose'
+friend = 'Rolf'
+phrase = name + ' is friends with ' + friend
+print(phrase)
+
+name = "Rolf Smith"
+street = "123 No Name Road"
+postcode = "PY10 1CP"
+address = f"""
+Name: {name}
+Street: {street}
+Postcode: {postcode}
+Country: United Kingdom """
+print(address)
+
+description = "{} is {} years old."
+"""
+print(Comments)
+"""
+print(description.format("Bob", 30))
